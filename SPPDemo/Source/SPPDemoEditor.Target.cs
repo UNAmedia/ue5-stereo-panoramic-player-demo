f9b46354f51ee235708c62fdc1d1204e71dc8b75
Daniele Benegiamo <daniele@unamedia.com>
2022-04-10 20:36:51 +0200

    Updated to UE5.0. Enabled all desktop VR plugins accordingly to Epic documentation (https://docs.unrealengine.com/5.0/en-US/developing-for-head-mounted-experiences-with-openxr-in-unreal-engine/ - Plugin Priority).

--- SPPDemo/Source/SPPDemoEditor.Target.cs
+++ SPPDemo/Source/SPPDemoEditor.Target.cs
@@ -8,6 +8,7 @@ public class SPPDemoEditorTarget : TargetRules
 	public SPPDemoEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
 
 		ExtraModuleNames.AddRange( new string[] { "SPPDemo" } );
 	}
